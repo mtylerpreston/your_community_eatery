eca5ea5cadf88318a4cbc26d9c0f10fc09409681
Tyler Preston <tylerpreston@Tylers-MacBook-Pro-3.local>
2019-07-22 09:10:18 -0700

    Minor updates

--- src/modeling.py
+++ src/modeling.py
@@ -62,8 +62,8 @@ all_ratings = []
 # Iterate over all algorithms
 for algorithm in [SVD(), surprise.SlopeOne(), surprise.NMF(),
                   surprise.NormalPredictor(), surprise.KNNBaseline(),
-                  # surprise.KNNBasic(), surprise.KNNWithMeans(),
-                  # surprise.KNNWithZScore(), surprise.BaselineOnly(),
+                  surprise.KNNBasic(), surprise.KNNWithMeans(),
+                  surprise.KNNWithZScore(), surprise.BaselineOnly(),
                   surprise.CoClustering()]:
 
     # Take a look at cross validation results to compare model types
